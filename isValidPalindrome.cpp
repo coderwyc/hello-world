c1470a1ef63887eb41ada6f0abafa2df31b1afdf
coderwyc <ucaswyc@126.com>
2015-01-12 15:16:32 +0800

    more concise version with same idea

--- isValidPalindrome.cpp
+++ isValidPalindrome.cpp
@@ -13,26 +13,18 @@ For the purpose of this problem, we define empty string as valid palindrome.
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string t = "";
-        for(int i=0; i<s.length(); i++)
-        {   
-            // processing string s before checking it is palindrome or not
-            if(s[i] >= 'a' && s[i] <= 'z' || s[i] >= '0' && s[i] <= '9' || s[i] >= 'A' && s[i] <= 'Z')
-            {
-                if(s[i] >= 'A' && s[i] <= 'Z') //Upper case to Lower case
-                    t += (s[i]-'A'+'a');
-                else
-                    t += s[i];   // string concatenation
-            }
-        }
-        if(t=="")
-            return true;
-        for(int i=0;i<t.length()/2;i++)
-        {
-            if(t[i] != t[t.length()-i-1])
+       string str;
+       for(int i = 0; i < s.length(); i++)
+       {
+            if(isalpha(s[i]) || isdigit(s[i]))
+                str.push_back(tolower(s[i]));        
+       }
+       
+       for(int i = 0; i < str.length()/2; i++)
+       {
+           if(str[i] != str[str.length()-i-1])
                 return false;
-        }
-        return true;
-        
+       }
+       return true;
     }
 };
