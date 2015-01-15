2dece64832ef4027c08d1dac10b53dd3fbae28ef
coderwyc <ucaswyc@126.com>
2015-01-15 20:31:02 +0800

    a better version

--- reverseWords.cpp
+++ reverseWords.cpp
@@ -1,47 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-class Solution
-{
-public:
-      void reverseWords(string &s)
-    {
-        vector<string> words;
-        int i = 0;
-        int len = s.size();
-        while(s[i] == ' ' && i < len) // leave out leading space
-            i++;
-        while(s[len-1] == ' ' && i < len) // leave out trailing space
-            len--;
-        // put word one by one into the words
-        while(i < len) {
-            int start = i;
-            if(s[start] != ' ') {
-                while(s[i] != ' ') // jump by one word
-                    i++;
-                words.push_back(s.substr(start , i - start));
-            } else {
-                while(s[i] == ' ') // jump space between word
-                    i++;
-                words.push_back(" ");
-            }
-        }
-        // string concatenation in reverse order
-        s = "";
-        for(int i = words.size() - 1; i >= 0; i--)
-            s += words[i];
-    }
-};
-int main()
-{
-    //cout<<"hello world!"<<endl;
-    string s = "  hello world my  ";
-    Solution so;
-    cout<<s<<endl;
-    so.reverseWords(s);
-    cout<<s<<endl;
-    return 0;
-}
-
+class Solution {  
+public:  
+	void reverseWords(string & s)  
+	{  
+	  
+		string ss;  
+		int i = s.length()-1;  
+		while(i>=0)  
+		{  
+			while(i>=0&&s[i] == ' ')  
+			{  
+				i --;  
+			}  
+			if(i<0) break;  
+			if(ss.length()!=0)  
+				ss.push_back(' ');  
+			string temp ;  
+			for(;i>=0&&s[i]!=' ';i--)  
+				temp.push_back(s[i]);  
+			reverse(temp.begin(),temp.end());  
+			ss.append(temp);  
+		}  
+		s=ss;  
+	}  
+	};
